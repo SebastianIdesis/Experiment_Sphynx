70351f4261fb74dd34f8b0dbd420bf83bfa8b8e7
Sebastian Idesis <68556202+SebastianIdesis@users.noreply.github.com>
2025-06-08 11:34:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+// Define study
+const study = lab.util.fromObject({
+  "title": "root",
+  "type": "lab.flow.Sequence",
+  "parameters": {},
+  "plugins": [
+    {
+      "type": "lab.plugins.Metadata",
+      "path": undefined
+    },
+    {
+      "type": "lab.plugins.Transmit",
+      "url": "backend.php",
+      "path": undefined
+    }
+  ],
+  "metadata": {
+    "title": "Testing for Sphynx",
+    "description": "",
+    "repository": "",
+    "contributors": ""
+  },
+  "files": {},
+  "responses": {},
+  "content": [
+    {
+      "type": "lab.canvas.Screen",
+      "content": [
+        {
+          "type": "i-text",
+          "left": 0,
+          "top": 0,
+          "angle": 0,
+          "width": 388.92,
+          "height": 36.16,
+          "stroke": null,
+          "strokeWidth": 1,
+          "fill": "black",
+          "text": "Welcome to the experiment",
+          "fontStyle": "normal",
+          "fontWeight": "normal",
+          "fontSize": 32,
+          "fontFamily": "sans-serif",
+          "lineHeight": 1.16,
+          "textAlign": "center"
+        }
+      ],
+      "viewport": [
+        800,
+        600
+      ],
+      "files": {},
+      "responses": {
+        "keypress": ""
+      },
+      "parameters": {},
+      "messageHandlers": {},
+      "title": "introScreen"
+    }
+  ]
+})
+
+// Let's go!
+study.run()
